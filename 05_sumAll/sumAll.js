8b845debc5bfde7c874d648be4d51eba4ceb82ac
nick <nicholasjpenaranda@gmail.com>
2022-07-18 20:08:05 -0500

    pass test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if (((numTwo | numOne) < 0) || (!Number.isInteger(numTwo)) || (!Number.isInteger(numOne))) {
+        return "ERROR";
+    };
+    let numArr = [numOne, numTwo];
+    let sum = 0;
+    numArr.sort(function(a, b){return a - b});
+    for (i = numArr[0]; i <= numArr[1]; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
