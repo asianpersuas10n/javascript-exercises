cb9515790ca1538fc7e64b5b66bce214d440d39d
nick <nicholasjpenaranda@gmail.com>
2022-07-18 19:10:00 -0500

    pass all test for 04_removeFromArray add all test so .skip is removed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    let newArr = arr;
+    for (let j = 0; j <= remove.length; j++) {
+        for (let i = 0; i <= arr.length; i++) {
+            if (arr[i] === remove[j]) {
+                newArr.splice(i, 1);
+            }
+        }
+    };
+    return newArr;
 };
 
 // Do not edit below this line
