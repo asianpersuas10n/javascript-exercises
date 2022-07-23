0bc46803ea0a75af1948c2c149bcad0515665768
nick <nicholasjpenaranda@gmail.com>
2022-07-23 06:21:56 -0500

    pass test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(...sum) {
+	let total = 0;
+  for (let i = 0; i < sum.length; i++) {
+    total += sum[i];
+  };
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(num, num2) {
+  let total = num - num2;
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(sum) {
+  let total = 0;
+  for (let i = 0; i < sum.length; i++) {
+    total += sum[i];
+  };
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(mult) {
+  let total = mult[0];
+  for (let i = 1; i < mult.length; i++) {
+    total *= mult[i];
+  };
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	let total = num;
+  for (let i = 1; i <= power - 1; i++) {
+    total *= num;
+  };
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total =  num;
+  for (let i = 1; i < num; i++) {
+    total *= (num - i);
+  };
+  if (num === 0) return 1;
+	return total;
 };
 
 // Do not edit below this line
