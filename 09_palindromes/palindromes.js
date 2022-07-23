6fbb30195bcd3eeef87fb0685f2e2bdd1cae2691
nick <nicholasjpenaranda@gmail.com>
2022-07-23 07:17:33 -0500

    pass test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const correctedStr = str.toLowerCase().replace(/[^a-z]/g, "");
+    return (correctedStr.split("").reverse().join("") == correctedStr);
 };
-
+console.log(palindromes("not a racecar"));
 // Do not edit below this line
 module.exports = palindromes;
